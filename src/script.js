9791af17c89e409203987b40ae1b786152536ba3
Brenton Beardsley <62326878+bbeardsley10@users.noreply.github.com>
2022-12-08 13:55:13 -0600

    Final changes

--- src/script.js
+++ src/script.js
@@ -1,86 +1,54 @@
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '271bb211cfmsh0ce82e9b880a6afp1401ebjsnd857d545f245',
-		'X-RapidAPI-Host': 'fifa-2022-schedule-and-stats.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "271bb211cfmsh0ce82e9b880a6afp1401ebjsnd857d545f245",
+    "X-RapidAPI-Host": "fifa-2022-schedule-and-stats.p.rapidapi.com",
+  },
 };
 
-const userInput = document.querySelector('input');
-const btn = document.querySelector('button');
+const userInput = document.querySelector("input");
+const btn = document.querySelector("button");
 
-
-let params = ''
+let params = "";
 
 const callParams = () => {
+  params = userInput.value;
+  fetch(
+    `https://fifa-2022-schedule-and-stats.p.rapidapi.com/schedule?date=${params}`,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      //let output = '';
+      //let output2 = '';
+      //  const homeTeam1 = []
+      //  const awayTeam1 = []
+      //console.log(data[0].matches[0]);
+      const list = data.matches;
+      //const list2 = data.matches.GroupName
 
-    params = userInput.value
-    fetch(`https://fifa-2022-schedule-and-stats.p.rapidapi.com/schedule?date=${params}`, options)
-        .then(response => response.json())
-        .then(data => {
-             //let output = '';
-             //let output2 = '';
-            //  const homeTeam1 = []
-            //  const awayTeam1 = []
+      let counter = 0;
+      list.map((match) => {
+        //console.log(match.matches);
+        //const homeTeam1 = match.Home.ShortClubName;
+        //const awayTeam1 = match.Away.ShortClubName;
+        console.log(match);
+        const homeTeam = match.Home.TeamName[0].Description;
+        const homeScore = match.Home.Score;
+        const awayTeam = match.Away.TeamName[0].Description;
+        const awayScore = match.Away.Score;
+        console.log(homeTeam);
+        console.log(awayTeam);
+        console.log(homeScore);
+        counter++;
+        const gameNumber = `<h4> Game ${counter}: </h4>`;
+        const game = `<h4> Home Team&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Away Team </h4><h4> ${homeTeam} (${homeScore})&nbsp;&nbsp; vs.&nbsp;&nbsp; ${awayTeam} (${awayScore})</h4><h4></h4>`;
+        document.querySelector(".Home").innerHTML += gameNumber;
+        document.querySelector(".Home").innerHTML += game;
+      });
+    })
+    .catch((err) => console.error(err));
+  userInput.value = "";
+};
 
-            const list = data.matches
-            //const list2 = data.matches.GroupName
-            let counter = 0
-            list.map((match) => {
-                //console.log(match)
-                const homeTeam1 = match.Home.ShortClubName
-                const awayTeam1 = match.Away.ShortClubName
-                const GroupName = match.GroupName
-                counter++
-                const gameNumber = `<h4> Game ${counter}: </h4>`
-                const game = `<h4> Home Team&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Away Team </h4><h4> ${homeTeam1}&nbsp;&nbsp; vs.&nbsp;&nbsp; ${awayTeam1}</h4>`
-                //console.log(homeTeam1)
-                //console.log(awayTeam1)
-                console.log(GroupName)
-                document.querySelector('.Home').innerHTML += gameNumber
-                document.querySelector('.Home').innerHTML += game
-            })
-
-            // list2.map((matches) => {
-            //     const GroupName = matches.GroupName.Description
-
-            //     console.log(GroupName)
-            // })
-            // data.matches.forEach(match => {
-                //console.log(match.Home.IdCountry)
-
-                // homeTeam1.push(match)
-                // awayTeam1.push(match.Away.IdCountry)
-
-                //  console.log(homeTeam1)
-                // console.log(awayTeam1)
-
-
-                // output += `
-                // <h4>
-                //     ${match.Home.IdCountry}
-                // </h4>
-                
-                // `
-                // output2 += `
-                // <h4>
-                //     ${match.Away.IdCountry}
-                // </h4>
-
-                //`
-                //})
-                //document.querySelector('.Home').innerHTML = "Home Team " + homeTeam1
-                    //document.querySelector('.Home').innerHTML = "Home Team " +output;
-                    //document.querySelector('.Away').innerHTML = "Away Team " + output2;
-                    // document.querySelector('.Home2').innerHTML = "Home Team " +output;
-                    // document.querySelector('.Away2').innerHTML = "Away Team " + output2;
-            })
-        .catch(err => console.error(err));
-    userInput.value = '';
-
-}
-
-
-btn.addEventListener('click', callParams)
-
-
+btn.addEventListener("click", callParams);
